0e65dfac560a04f8df8a9dff4a9980a10abc0835
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-27 13:05:42 +0800

    feat: login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import {user, login} from "./src/models/users_models.js"
 import routers from "./src/routers/index.js"
+import {constants} from "node:http2"
 
 const app = express()
 
@@ -9,19 +10,20 @@ console.log(user)
 app.post("/login", (req, res) => {
    const {email, password} = req.body
    console.log(email)
-//    user.forEach(u => {
-//         if (email === u.email) {
-//             res.json({
-//                 success: true,
-//                 message: `Wellcome ${user[i].name} !`
-//             })
-//             return
-//         }
-//         res.status(constants.HTTP_STATUS_UNAUTHORIZED).json({
-//             success: false,
-//             message:"Wrong Email or PAssword"
-//         })
-//     });
+   user.forEach(u => {
+    console.log(u.email)
+        if (email === u.email && password === u.password) {
+            res.json({
+                success: true,
+                message: `Wellcome ${u.name} !`
+            })
+            return
+        }
+        res.status(constants.HTTP_STATUS_UNAUTHORIZED).json({
+            success: false,
+            message:"Wrong Email or Password"
+        })
+    });
 })
 
 app.listen(8080,()=> {
