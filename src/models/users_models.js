b65afefd444ccc4832da555788292c32358f6a11
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-27 17:59:26 +0800

    fix: func update user

--- src/models/users_models.js
+++ src/models/users_models.js
@@ -49,28 +49,21 @@ function changeUser(emailUser, email, name) {
     let results = user.findIndex(u => u.email === emailUser)
     console.log(results)
     if (results !== -1) {
-        
-    } 
-    user[results].email = email
-    user[results].name = name
-    
-    // user.forEach(element => {
-    //     if (element.email === emailUser) {
-    //         element.email = email
-    //         element.name =name
-    //         return {
-    //             status: true,
-    //             message: `Updated ${emailUser} success`,
-    //             data: element
-    //         }
-    //     }
-    // });
-
-    return {
-        status: false,
-        message: `Updated ${emailUser} failed, user not found`,
-        data: {}
+        user[results].email = email
+        user[results].name = name
+        return {
+            status: true,
+            message: `Updated ${emailUser} success`,
+            data: user
+        }
+    } else {
+        return {
+            status: false,
+            message: `Updated ${emailUser} failed, user not found`,
+            data: {}
+        }
     }
+
 }
 
 export { 
