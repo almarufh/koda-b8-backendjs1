fee2e6c07a53616ad5c0f2a1a5ce9761fa6090b3
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-28 07:40:11 +0800

    feat: used parameter limit and page

--- src/models/users_models.js
+++ src/models/users_models.js
@@ -1,5 +1,7 @@
 import fs from "fs"
 import path from "path"
+import { sep } from "path/win32";
+import { DEFAULT_CIPHERS } from "tls";
 
 const FILE_PATH = path.join(process.cwd(), 'database', 'user.json');
 
@@ -111,11 +113,27 @@ function deleteUser(emailUser, email, name) {
 
 }
 
+function searchUser (search, limit, page) {
+    limit = parseInt(limit) || 5
+    page = parseInt(page) || 1
+    let start = (page - 1) * limit
+    let res = user.filter(user => 
+        user.email.toLowerCase().includes(search.toLowerCase())  || user.name.toLowerCase().includes(search.toLowerCase())
+    )
+    return {
+        success: true,
+        message: `${res.length} users found !`,
+        data: res.slice(start, start + limit)
+    }
+}
+
+
 export { 
     user, 
     createUser,
     getUsers,
     DetailUser,
     changeUser,
-    deleteUser
+    deleteUser,
+    searchUser
 }
