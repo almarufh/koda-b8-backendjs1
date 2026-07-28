bc030c7d853421b3b9fc80c253225e75bb4a8306
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-28 12:29:17 +0800

    feat: implementastion middleware in users

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import authRouter from "./auth_routers.js"
 import userRouter from "./user_routers.js";
+import {authMiddleware} from "../middlewares/auth.js"
+import {corsMiddleware} from "../middlewares/cors.js"
 
 const routers = Router()
+routers.use(authMiddleware)
 
 routers.use("/auth", authRouter)
-routers.use("/user", userRouter)
+routers.use("/user", corsMiddleware, userRouter)
 
 export default routers
